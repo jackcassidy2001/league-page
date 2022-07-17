27a42db4f4d732f849431e22821684551e14922d
jackcassidy2001 <95175229+jackcassidy2001@users.noreply.github.com>
2022-07-17 15:23:36 -0500

    Update leagueInfo.js
    Signed-off-by: jackcassidy2001 <95175229+jackcassidy2001@users.noreply.github.com>

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -212,7 +212,7 @@ export const managers = [
       "rival": {
         name: "Joseph Ruhland", // Can be anything (usually your rival's name)
         link: 7, // manager array number within this array, or null to link back to all managers page
-        image: "/managers/question.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+        image: "joey.jpeg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 5937, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
